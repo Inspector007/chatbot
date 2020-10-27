b77ac34329c6b3c0192fe3c61d136b12bbcbb9a0
inspector007 <ds1990singh@yahoo.com>
2020-10-27 09:15:53 +0530

    intelligent mll bot

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from utility import fetch_reply
 
 app = Flask(__name__)
 
@@ -9,11 +10,17 @@ def index():
 
 @app.route("/sms", methods=['POST'])
 def reply():
+
+    # Fetch the message
     msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg, phone_no)
+    """
     if(msg.lower() == "hello"):
     	reply = "Hi! \n How can i help you?" 
     else:
     	reply = "Hi! \nI am MLL Virtual Assistance.\nI am in developing mode.\nThanks."
+    """
     response = MessagingResponse()
     response.message(reply)
 
