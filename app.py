cd17c68e9e92a24a51221f100bbca0c6e12e5289
inspector007 <ds1990singh@yahoo.com>
2020-10-30 15:12:46 +0530

    added kpi insertion code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from utility import fetch_reply
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -9,23 +11,47 @@ def index():
 	return "Hello, World!"
 
 
+
+def kpiinsertion(contactno,userid,kpicount,kpidate):
+	API_ENDPOINT = "https://quiet-brook-50483.herokuapp.com/api/v1/adduserkpidetail/"
+	data = {
+    	"userid": userid,
+    	"contactno": contactno,
+    	"kpicount": kpicount,
+    	"kpidate": kpidate
+	}
+	r = requests.post(url = API_ENDPOINT, data = data)
+	return r.text
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
 	req = request.get_json(silent=True, force=True)
+	phone_no = request.form.get('From')
 	fulfillmentText = ''
 	sum = 0
 	query_result = req.get('queryResult')
 	if query_result.get('action') == 'orderinfo':
 		num1 = int(query_result.get('parameters').get('number'))
-		print('here num1 = {0}'.format(num1))
 		fulfillmentText = 'The order numbers is {0} and \n and It is under processing !!!'.format(num1)
 		return {
 		"fulfillmentText": fulfillmentText,
 		"displayText": '25',
 		"source": "webhookdata"
-	}
-
-	# Fetch the message
+		}
+	if query_result.get('action') == 'addkpi':
+		kpicount = int(query_result.get('parameters').get('number'))
+		datestr = str(query_result.get('parameters').get('date'))
+		print('here num1 = {0}'.format(datestr[:10]))
+		kpidate = datetime.strptime(datestr[:10],'%Y-%m-%d').date()
+		userid = '1111'
+		message = kpiinsertion(phone_no,userid,kpicount,kpidate)
+		fulfillmentText = 'Your kpi {0} on \n {1} is captured!!!'.format(kpicount,kpidate)
+		return {
+		"fulfillmentText": fulfillmentText,
+		"displayText": '25',
+		"source": "webhookdata"
+		}
 	msg = request.form.get('Body')
 	phone_no = request.form.get('From')
 	reply = fetch_reply(msg, phone_no)
@@ -46,6 +72,7 @@ def reply():
 	msg = request.form.get('Body')
 	phone_no = request.form.get('From')
 	reply = fetch_reply(msg, phone_no)
+	print(phone_no+' aaya yha pe')
 	"""
 	if(msg.lower() == "hello"):
 		reply = "Hi! \n How can i help you?" 
